9d7e2e36b571bdc12755af4bdd88611220ac0053
jmperezs1 <jm.perezs1@uniandes.edu.co>
2022-02-20 19:18:22 -0500

    primero

--- Codigo/parser.py
+++ Codigo/parser.py
@@ -4,6 +4,16 @@ archivo = open(cf.data_dir + 'prueba.txt',encoding="utf-8" )
 
 def main(archivo):
     for linea in archivo:
+        respuesta = False
+        checkLine = check(linea)
+
+        if checkLine == 1:
+            respuesta = True
+
+        else:
+            break
+
+    print(respuesta)
         
         
 main(archivo)
